ef46cf77e69c89640104092a041a674f9e465824
samisafatli <samisafatli@live.com>
2020-03-01 17:18:34 -0300

    Fix bug: Button can be clicked only once

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -2,18 +2,25 @@ import React, { useState } from 'react'
 
 const QuestionBox = ({question,options, selected}) => {
     const [answer, setAnswer] = useState(options)
+    function clickAnswer(text) {
+        if (answer.length > 1) {
+          setAnswer([text]);
+          selected(text);
+        }
+      }
 
     return(
         <div className="questionBox">
             <div className="question">{question}</div>
                 {answer.map((text, index) => (
-                    <button key={index} 
+                    <button
+                        key={index} 
                         className="answerBtn" 
                         onClick={() => {
-                            setAnswer([text])
-                            selected(text)
-                    }}>
-                        {text}
+                            clickAnswer(text);
+                            }
+                        }>
+                    {text}
                     </button>
                 ))}
         </div>
